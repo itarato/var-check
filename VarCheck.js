6d8735a2fe7fb5796aaeef0c8a4c6bf55ecc1b92
Peter Arato <it.arato@gmail.com>
2013-11-07 22:38:53 +0000

    Add JS implementation.

---
+++ VarCheck.js
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * JavaScript implementation of VarCheck.
+ */
+
+/**
+ * Provides chained facility to check if a nested attribute exists, and returns the value.
+ *
+ * @param value
+ *  Root of the variable.
+ * @returns {{key: Function, value: Function}}
+ *  Object for chaining. [key] to go a level deeper in the variable. [value] to extract the actual value.
+ * @constructor
+ *
+ * Usage:
+ * @code
+ * var obj = {
+ *   foo: [1, 2, 3],
+ *   bar: {}
+ * };
+ * var_check(obj).key('foo').key(1).value(); // 2
+ * var_check(obj).key('foo').key(6).value(); // undefined
+ * var_check(obj).key('bar').value(); // {}
+ * var_check(obj).key('foobar').key(1).value(); // undefined
+ * var_check(obj).key(1).key('foo').key('foo').value(); // undefined
+ * @endcode
+ */
+var_check = function (value) {
+  return {
+    key: function (attr) {
+      return value && value.hasOwnProperty(attr) ? var_check(value[attr]) : var_check(undefined);
+    },
+    value: function () {
+      return value;
+    }
+  };
+};
